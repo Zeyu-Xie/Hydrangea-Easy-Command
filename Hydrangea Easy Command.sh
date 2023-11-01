bc08fc7e43a72eb3468e586d8bbe9574924e4785
Acan <xie.zeyu20@gmail.com>
2023-11-01 17:43:52 +0800

    .

---
+++ Hydrangea Easy Command.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+source ~/.zshrc
+source ~/.zprofile
+
+# The path of the config file
+srcPath="./commands.json"
+
+if command -v jq &>/dev/null; then
+    # Nothing happens lol
+else
+    echo "Start installing jq"
+    brew install jq
+fi
+
+num=$(jq ". | length" $srcPath)
+
+for ((i = 0; i < num; i++)); do
+    if [ "$(jq ".[$i].alias" $srcPath)" = "\"$1\"" ]; then
+        eval $(jq ".[$i].command" $srcPath)
+    fi
+done
